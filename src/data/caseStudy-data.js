7cd17fb3199d56f046c427a88ba14a5e7123f508
Allison Leggett <alliemack77@yahoo.com>
2022-02-24 10:05:44 -0800

    testing homepage photos

--- src/data/caseStudy-data.js
+++ src/data/caseStudy-data.js
@@ -1,10 +1,10 @@
-import neebsSmall from '../images/arkAll5-small.png';
-import musicSmall from '../images/music-small.png'
-import beehiveSmall from '../images/dev-group-small.png'
-import skateSmall from '../images/skate-small.png'
+import neebsSmall from '../images/ark-small.jpg';
 import neebsLrg from '../images/arkAll5-lrg.png';
+import musicSmall from '../images/music-small.jpg'
 import musicLrg from '../images/music-lrg.png'
+import beehiveSmall from '../images/dev-group-small.jpg'
 import beehiveLrg from '../images/dev-group-lrg.jpg'
+import skateSmall from '../images/skate-small.jpg'
 import skateLrg from '../images/skate-lrg.png'
 
 export const caseStudies = [
